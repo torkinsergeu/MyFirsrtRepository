b5ada5c0d05228b9f20e66940ff333d502abe847
Hosaph <torkinsergeu@gmail.com>
2025-06-11 21:57:37 +0300

    test Commit

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+def factorial(n:int) -> int:
+  x = 1
+  for i in range(n):
+    x*=n
+    n-=1
+  return x
+
 def main():
   print("Hello, world!")
+  print(factorial(5))
 if __name__ == "__main__":
   main()
