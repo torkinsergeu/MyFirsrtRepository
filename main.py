32afa030de00982a84eac0d01ba107fc557abf28
Hosaph <torkinsergeu06@gmail.com>
2025-06-23 13:05:23 +0300

    haha

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-def chlen(n:int):
-  lst =[['']*4 for i in range(n+1)]
-  for i in range(len(lst)):
-    lst[i][1] = "| "
-    lst[i][2] = "| "
-  lst[-1] = ["(",")","(",")",]
-  for x in lst:
-    print(x)
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message
+from aiogram.filters import Command
+from environs import Env
 
-def main():
-  chlen(5)
-  
+env = Env()
+env.read_env()
+TOKEN = env("TOKEN")
+
+bot = Bot(TOKEN)
+dp = Dispatcher()
+
+@dp.message(Command("start"))
+async def start(message: Message):
+  await message.answer("Hello!")
+
 if __name__ == "__main__":
-  main()
+  dp.run_polling(bot)
